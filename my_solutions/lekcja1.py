3c28714bc264e22204756ee2640b5985ea109237
Jan Zieleznicki <janek.zieleznicki@gmail.com>
2016-05-04 19:34:05 +0200

    JZ

--- my_solutions/lekcja1.py
+++ my_solutions/lekcja1.py
@@ -5,6 +5,7 @@ import numpy as np
 def nazwa(tekst1, tekst2):
     print(tekst1, tekst2)
     myfunction()
+
 def myfunction():
     a = 5.
     print()
@@ -14,4 +15,27 @@ def myfunction():
     print(str(i))
 
 
-nazwa('aaa', 'bbb')
+def listy():
+    a = []
+    # b = list[20]
+    print(isinstance(a, list))
+    # print(isinstance(b,list))
+    # print(b)
+    print(a)
+    for i in range(0, 11):
+        a.append(i)
+    print(a)
+
+
+def foryify():
+    a = [100, scp.pi, scp.e, 'cos']
+    for element in a:
+        print(element)
+    b = [i ** 2 for i in range(101)]
+    print(b)
+
+
+listy()
+a = [1, 2, 3, 'cos', scp.pi, np.pi, np.pi - scp.pi]
+print(a)
+foryify()
