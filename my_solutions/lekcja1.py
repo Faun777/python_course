2eb1ede5c7b52ce403b7e7292220e90e577a9876
Jan Zieleznicki <janek.zieleznicki@gmail.com>
2016-05-04 19:11:21 +0200

    JZ

--- my_solutions/lekcja1.py
+++ my_solutions/lekcja1.py
@@ -2,11 +2,16 @@ import scipy as scp
 import numpy as np
 
 
+def nazwa(tekst1, tekst2):
+    print(tekst1, tekst2)
+    myfunction()
 def myfunction():
-    a = 5
-    for i in range(0, 100):
-        a = a + 1
+    a = 5.
+    print()
+    for i in range(1, 100):
+        a = a + 1 / (np.sqrt(i))
     print("Moja liczba : " + str(a))
+    print(str(i))
 
 
-myfunction()
+nazwa('aaa', 'bbb')
