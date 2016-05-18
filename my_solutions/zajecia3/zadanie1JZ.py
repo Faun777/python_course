bf6c24b2f7e45f1e5158a619f37492283aa100a1
Jan Zieleznicki <janek.zieleznicki@gmail.com>
2016-05-18 20:05:10 +0200

    JZ

--- my_solutions/zajecia3/zadanie1JZ.py
+++ my_solutions/zajecia3/zadanie1JZ.py
@@ -13,8 +13,35 @@ def animate_contour_plot(framesDatas, sizeX=(0,1), sizeY=(0,1), dataRange=None, 
     import numpy as np
     import matplotlib.pyplot as plt
     from matplotlib import animation
-    pass
+    nx, ny = framesDatas[0].shape
 
+    Z0 = framesDatas[0]
+    if dataRange:
+        minVal,maxVal=dataRange
+    else:
+        minVal = min(Z0.flatten())
+        maxVal = max(Z0.flatten())
+
+    x = np.linspace(sizeX[0],sizeX[1],nx)
+    y = np.linspace(sizeY[0],sizeY[1],ny)
+    X,Y = np.meshgrid(x,y)
+    fig = plt.figure()
+    cs=plt.contourf(X,Y,Z0,nLevels)
+    cs.zmax=maxVal
+    cs.zmin=minVal
+    plt.colorbar(cs,ticks=np.linspace(minVal,maxVal,nLevels+1))
+    if len(framesDatas)>1:
+    #     tworzymy animacje
+        def animate(nFrame):
+            # nFrame=nFrame*skip
+            Z = framesDatas[nFrame]
+            cs= plt.contourf(X,Y,Z,nLevels)
+            cs.zmax=maxVal
+            cs.zmin=minVal
+            # plt.colorbar(cs,ticks=np.linspace(minVal,maxVal,nLevels+1))
+            return cs
+        anim = animation.FuncAnimation(fig,animate,frames=int(len(framesDatas)/skip),repeat=repeat)
+    plt.show()
 
 #TEST (Waring below lines should be commented out when this module will be used in other files)
 import numpy as np
@@ -24,4 +51,4 @@ Z = []
 for t in np.linspace(0, 2*np.pi, 50):
     Z.append( np.exp(2*X*Y)*np.sin(t) )
 
-animate_contour_plot(Z, dataRange=(-10.,10.))
+animate_contour_plot(Z, nLevels=20,dataRange=(-10.,10.))
